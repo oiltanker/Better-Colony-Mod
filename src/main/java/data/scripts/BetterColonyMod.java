b82477ba417b9abb00372b880c26753be5e17b3c
BuildTools <unconfigured@null.spigotmc.org>
2019-10-26 14:33:58 +0300

    final balance changes

--- src/main/java/data/scripts/BetterColonyMod.java
+++ src/main/java/data/scripts/BetterColonyMod.java
@@ -8,13 +8,8 @@ import com.fs.starfarer.api.impl.campaign.population.CoreImmigrationPluginImpl;
 import com.fs.starfarer.api.impl.campaign.econ.impl.PopulationAndInfrastructure;
 
 public class BetterColonyMod extends BaseModPlugin {
-    public static float[] USE_RATE = {
-        // 0.5f, 0.25f, 0.125f, 0.062f, 0.0405f, 0.0335f, 0.0295f, 0.0265f, 0.0225f, 0.0f
-        0.5f, 0.25f, 0.125f, 0.125f, 0.125f, 0.125f, 0.125f, 0.125f, 0.125f, 0.125f
-    };
-    public static int[] MAX_IND = {
-        1, 1, 1, 2, 3, 4, 5, 6, 7, 8
-    };
+    public static float[] USE_RATE = { 0.1464f, 0.1357f, 0.1250f, 0.1143f, 0.1036f, 0.0929f, 0.0821f, 0.0714f, 0.0607f, 0.0500f };
+    public static int[] MAX_IND = { 1, 1, 1, 2, 3, 4, 5, 6, 7, 8 };
     public static Logger logger = Global.getLogger(BetterColonyMod.class);
 
     @Override
