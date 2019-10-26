1d4732e79ce14b6491c91f80bb735c2dfa7fe786
BuildTools <unconfigured@null.spigotmc.org>
2019-10-27 01:22:48 +0300

    versioning stub

--- src/main/java/com/github/bettercolony/BetterColonyMod.java
+++ src/main/java/com/github/bettercolony/BetterColonyMod.java
@@ -18,9 +18,11 @@ import com.fs.starfarer.api.impl.campaign.procgen.themes.SectorThemeGenerator;
 import com.fs.starfarer.api.impl.campaign.procgen.themes.ThemeGenContext;
 
 public class BetterColonyMod extends BaseModPlugin {
-    public static float[] USE_RATE = { 0.1464f, 0.1357f, 0.1250f, 0.1143f, 0.1036f, 0.0929f, 0.0821f, 0.0714f, 0.0607f,
-            0.0500f };
+    public static String VERSION = "1.0.0";
+
+    public static float[] USE_RATE = { 0.1464f, 0.1357f, 0.1250f, 0.1143f, 0.1036f, 0.0929f, 0.0821f, 0.0714f, 0.0607f, 0.0500f };
     public static int[] MAX_IND = { 1, 1, 1, 2, 3, 4, 5, 6, 7, 8 };
+
     public static Logger logger = Global.getLogger(BetterColonyMod.class);
 
     @Override
@@ -38,9 +40,13 @@ public class BetterColonyMod extends BaseModPlugin {
         MemoryAPI memory = Global.getSector().getMemory();
         if (newGame) {
             memory.set("$better_colony_mod", true);
+            memory.set("$better_colony_mod.version", VERSION);
         } else {
-            Object memObj = memory.get("$better_colony_mod");
-            boolean already_generated = (memObj != null) ? (boolean) memObj : false;
+            Object genObj = memory.get("$better_colony_mod");
+            Object verObj = memory.get("$better_colony_mod.version");
+            boolean already_generated = (genObj != null) ? (boolean) genObj : false;
+            String generated_version = (verObj != null) ? (String) verObj : null;
+
             if (!already_generated) {
                 // Prepare context
                 ThemeGenContext context = new ThemeGenContext();
@@ -62,6 +68,12 @@ public class BetterColonyMod extends BaseModPlugin {
 
                 // set already generated
                 memory.set("$better_colony_mod", true);
+                memory.set("$better_colony_mod.version", VERSION);
+            }
+
+            if (!VERSION.equals(generated_version)) {
+                // TODO: Implement support for different save versions
+                memory.set("$better_colony_mod.version", VERSION);
             }
         }
     }
