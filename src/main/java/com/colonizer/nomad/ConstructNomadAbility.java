17b4ece03ad40cc9568950fe5c4ba80f4e857365
BuildTools <unconfigured@null.spigotmc.org>
2019-11-16 02:25:24 +0200

    experiments

--- src/main/java/com/colonizer/nomad/ConstructNomadAbility.java
+++ src/main/java/com/colonizer/nomad/ConstructNomadAbility.java
@@ -1,31 +1,103 @@
 package com.colonizer.nomad;
 
+import com.colonizer.nomad.dialog.NomadBuildingDialogPlugin;
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.CampaignFleetAPI;
+import com.fs.starfarer.api.campaign.LocationAPI;
+import com.fs.starfarer.api.campaign.SectorEntityToken;
+import com.fs.starfarer.api.campaign.ai.CampaignFleetAIAPI;
+import com.fs.starfarer.api.campaign.econ.Industry;
+import com.fs.starfarer.api.campaign.econ.MarketAPI;
+import com.fs.starfarer.api.campaign.econ.MarketConditionAPI;
+import com.fs.starfarer.api.campaign.econ.MarketAPI.SurveyLevel;
+import com.fs.starfarer.api.fleet.FleetMemberType;
+import com.fs.starfarer.api.impl.campaign.submarkets.StoragePlugin;
 import com.fs.starfarer.api.impl.campaign.abilities.BaseDurationAbility;
+import com.fs.starfarer.api.impl.campaign.fleets.FleetFactory;
+import com.fs.starfarer.api.impl.campaign.ids.Conditions;
+import com.fs.starfarer.api.impl.campaign.ids.Factions;
+import com.fs.starfarer.api.impl.campaign.ids.*;
 
 public class ConstructNomadAbility extends BaseDurationAbility {
+    CampaignFleetAPI playerFleet;
+    LocationAPI playerLocation;
+
+    public ConstructNomadAbility() {
+        playerFleet = Global.getSector().getPlayerFleet();
+        playerLocation = playerFleet.getContainingLocation();
+    }
 
     @Override
     protected void activateImpl() {
-        // TODO Auto-generated method stub
+        // // Fleet
+        // CampaignFleetAPI nomadFleet = FleetFactory.createGenericFleet(Factions.PLAYER, "Nomad fleet", 1f, 100);
+        // nomadFleet.setContainingLocation(playerLocation);
+        // nomadFleet.setLocation(playerFleet.getLocation().x, playerFleet.getLocation().y);
+        // for (SectorEntityToken entity: playerLocation.getAllEntities()) {
+        //     if (entity.isStar()) {
+        //         playerLocation.spawnFleet(entity, playerFleet.getLocation().x, playerFleet.getLocation().y, nomadFleet);
+        //         break;
+        //     }
+        // }
+        // nomadFleet.setTransponderOn(true);
 
+        // Market
+        MarketAPI market = Global.getFactory().createMarket("market_nomad", "Nomad market", 3);
+
+        market.setSize(3);
+        market.setHidden(false);
+        market.setFactionId(Factions.PLAYER);
+        market.addCondition(Conditions.POPULATION_3);
+        market.addIndustry(Industries.POPULATION);
+        market.addIndustry(Industries.SPACEPORT);
+
+        market.getTariff().modifyFlat("default_tariff", market.getFaction().getTariffFraction());
+
+        market.setSurveyLevel(SurveyLevel.FULL);
+        for (MarketConditionAPI condition : market.getConditions())
+            condition.setSurveyed(true);
+        market.setPlayerOwned(true);
+
+        market.addSubmarket(Submarkets.LOCAL_RESOURCES);
+        market.addSubmarket(Submarkets.SUBMARKET_STORAGE);
+        ((StoragePlugin) market.getSubmarket(Submarkets.SUBMARKET_STORAGE).getPlugin()).setPlayerPaidToUnlock(true);
+        for (Industry industry:  market.getIndustries()) {
+            industry.doPreSaveCleanup();
+            industry.doPostSaveRestore();
+        }
+
+        // Fleet
+        CampaignFleetAPI nomadFleet = FleetFactory.createEmptyFleet(Factions.PLAYER, "Nomad fleet", market);
+        nomadFleet.setContainingLocation(playerLocation);
+        nomadFleet.setLocation(playerFleet.getLocation().x, playerFleet.getLocation().y);
+        nomadFleet.setTransponderOn(true);
+        for (SectorEntityToken entity: playerLocation.getAllEntities()) {
+            if (entity.isStar()) {
+                playerLocation.spawnFleet(entity, playerFleet.getLocation().x, playerFleet.getLocation().y, nomadFleet);
+                break;
+            }
+        }
+        nomadFleet.getFleetData().addFleetMember(Global.getFactory().createFleetMember(FleetMemberType.SHIP, "legion_Escort"));
+
+        // Finalizing
+        market.setPrimaryEntity(nomadFleet);
+        market.setAdmin(Global.getSector().getPlayerPerson());
+        nomadFleet.setMarket(market);
+        Global.getSector().getEconomy().addMarket(market, true);
+
+        Global.getSector().getCampaignUI().showInteractionDialog(new NomadBuildingDialogPlugin(), playerFleet);
     }
 
     @Override
     protected void applyEffect(float amount, float level) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     protected void deactivateImpl() {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     protected void cleanupImpl() {
-        // TODO Auto-generated method stub
-
     }
 
 }
---
+++ src/main/java/com/colonizer/nomad/dialog/NomadBuildingDialogPlugin.java
@@ -0,0 +1,89 @@
+package com.colonizer.nomad.dialog;
+
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.LocationAPI;
+import com.fs.starfarer.api.campaign.SectorEntityToken;
+import com.fs.starfarer.api.campaign.econ.Industry;
+import com.fs.starfarer.api.campaign.econ.MarketAPI;
+import com.fs.starfarer.api.campaign.econ.MarketConditionAPI;
+import com.fs.starfarer.api.campaign.econ.MarketAPI.SurveyLevel;
+import com.fs.starfarer.api.impl.campaign.ids.Conditions;
+import com.fs.starfarer.api.impl.campaign.ids.Factions;
+import com.fs.starfarer.api.impl.campaign.ids.Submarkets;
+import com.fs.starfarer.api.impl.campaign.submarkets.StoragePlugin;
+import com.colonizer.config.Stations;
+import com.colonizer.dialog.BaseInteractionDialogPlugin;
+import com.colonizer.dialog.DefaultingInteractionDialogPlugin;
+import com.colonizer.dialog.Option;
+
+import org.lwjgl.input.Keyboard;
+
+import com.fs.starfarer.api.loading.Description;
+import com.fs.starfarer.api.campaign.InteractionDialogPlugin;
+
+public class NomadBuildingDialogPlugin extends BaseInteractionDialogPlugin<NomadBuildingDialogPlugin.NomadOption> {
+    public static class NomadOption extends BaseInteractionDialogPlugin.BaseOption {
+		public static String BUILD_NOMAD = Option.newOption();
+        public static String CONFIRM_BUILD = Option.newOption();
+    }
+
+    @Override
+    public void optionSelectedImpl(String option, String optionText) {
+        if (option == NomadOption.INIT) {
+            if (target.getCustomDescriptionId() != null) {
+                addText(Global.getSettings().getDescription(
+                    target.getCustomDescriptionId(), Description.Type.CUSTOM).getText1());
+            }
+
+            visual.showImagePortion("illustrations", "free_orbit", 480, 300, 0, 0, 480, 300);
+            addText("You decide to ...");
+
+            options.addOption("Assemble nomad construction site", NomadOption.BUILD_NOMAD);
+            options.addOption("Leave", NomadOption.LEAVE);
+            options.setShortcut(NomadOption.LEAVE, Keyboard.KEY_ESCAPE, false, false, false, false);
+        } else if (option == NomadOption.BUILD_NOMAD) {
+            visual.showImagePortion("illustrations", "orbital_construction", 480, 300, 0, 0, 480, 300);
+            addText("Construction of a mining station would require following resources ...");
+
+            options.addOption("Proceed with building the station", NomadOption.CONFIRM_BUILD);
+            options.addOption("Never mind", NomadOption.INIT);
+
+            if (!showCost("Resources: required (available)", Stations.MINING.cost, false) && !Global.getSettings().isDevMode())
+                options.setEnabled(NomadOption.CONFIRM_BUILD, false);
+        } else if (option == NomadOption.CONFIRM_BUILD) {
+            chargeCost(Stations.MINING.cost);
+            
+            SectorEntityToken station = replaceEntity(playerFleet.getStarSystem(), target, Stations.MINING.type, Factions.NEUTRAL);
+            LocationAPI location = station.getContainingLocation();
+            station.setName(location.getNameWithTypeIfNebula() + " " + station.getCustomEntitySpec().getNameInText());
+
+            MarketAPI market = Global.getFactory().createMarket("market_" + station.getId(), station.getName(), 0);
+
+            market.setFactionId(Factions.NEUTRAL);
+            market.setHidden(false);
+            market.setSize(0);
+            market.addCondition(Conditions.ABANDONED_STATION);
+            market.addCondition(Conditions.ORE_MODERATE);
+            market.addCondition(Conditions.RARE_ORE_SPARSE);
+
+            market.addSubmarket(Submarkets.SUBMARKET_STORAGE);
+            ((StoragePlugin) market.getSubmarket(Submarkets.SUBMARKET_STORAGE).getPlugin()).setPlayerPaidToUnlock(true);
+            for (Industry industry:  market.getIndustries()) {
+                industry.doPreSaveCleanup();
+                industry.doPostSaveRestore();
+            }
+
+            market.setSurveyLevel(SurveyLevel.FULL);
+            for (MarketConditionAPI condition : market.getConditions()) condition.setSurveyed(true);
+
+            market.setPrimaryEntity(station);
+            station.setMarket(market);
+            Global.getSector().getEconomy().addMarket(market, false);
+
+            dialog.setInteractionTarget(station);
+            InteractionDialogPlugin plugin = new DefaultingInteractionDialogPlugin();
+            dialog.setPlugin(plugin);
+            plugin.init(dialog);
+        }
+    }
+}
